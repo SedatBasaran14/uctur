0f18326466e1874c9c1c06fb753ae5fce6058766
unknown <>
2021-01-20 16:42:20 +0300

    v 0.0

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app     = express();
+app.set("view engine" , "ejs");
+app.use(express.static(__dirname + "/dosyalar"));
+
+
+
+
+
+
+
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+
+
+let port = process.env.PORT;
+if(port == "" || port == null){
+  port = 5000;
+}
+app.listen(port, function(){
+  console.log("port numarasi : " + port);
+});
